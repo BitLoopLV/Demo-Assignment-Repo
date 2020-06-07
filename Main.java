0dfe05d9ccc71208828b0762dc3f1e46d413e95f
BitLoopLV <66575644+BitLoopLV@users.noreply.github.com>
2020-06-07 16:22:32 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public void solveProblem(String input) {
+    //Write your solution here and return the answer
+  }
+  public static void main(String[] args) {
+      //Don't touch this code
+      System.out.println(solveProblem(args[0]));
+  }
+}
